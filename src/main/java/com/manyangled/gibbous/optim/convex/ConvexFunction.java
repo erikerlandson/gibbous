1f645549c01554861a39f44eebe83da288994b58
Erik Erlandson <eerlands@redhat.com>
2018-05-03 10:15:48 -0700

    ConvexFunction

---
+++ src/main/java/com/manyangled/gibbous/optim/convex/ConvexFunction.java
@@ -0,0 +1,19 @@
+
+import org.apache.commons.math3.analysis.MultivariateFunction;
+
+public abstract class ConvexFunction implements MultivariateFunction {
+    abstract void grad(double[] x, double[] g);
+    abstract void hess(double[] x, double[][] h);
+
+    double[] grad(double[] x) {
+        double[] g = new double[x.length];
+        grad(x, g);
+        return g;
+    }
+
+    double[][] hess(double[] x) {
+        double[][] h = new double[x.length][x.length];
+        hess(x, h);
+        return h;
+    }
+}
