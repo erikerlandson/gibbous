63543ca81bd47f2f0de9192e7fe8d3229f26e7e5
Erik Erlandson <eerlands@redhat.com>
2018-05-03 10:33:10 -0700

    make x final

--- src/main/java/com/manyangled/gibbous/optim/convex/ConvexFunction.java
+++ src/main/java/com/manyangled/gibbous/optim/convex/ConvexFunction.java
@@ -3,16 +3,16 @@ package com.manyangled.gibbous.optim.convex;
 import org.apache.commons.math3.analysis.MultivariateFunction;
 
 public abstract class ConvexFunction implements MultivariateFunction {
-    abstract void grad(double[] x, double[] g);
-    abstract void hess(double[] x, double[][] h);
+    abstract void grad(final double[] x, double[] g);
+    abstract void hess(final double[] x, double[][] h);
 
-    double[] grad(double[] x) {
+    double[] grad(final double[] x) {
         double[] g = new double[x.length];
         grad(x, g);
         return g;
     }
 
-    double[][] hess(double[] x) {
+    double[][] hess(final double[] x) {
         double[][] h = new double[x.length][x.length];
         hess(x, h);
         return h;
