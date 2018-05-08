0affc16c19859ded8b0082b699eab581398b020b
Erik Erlandson <eerlands@redhat.com>
2018-05-08 10:16:32 -0700

    EqualityConstraint

---
+++ src/main/java/com/manyangled/gibbous/optim/convex/EqualityConstraint.java
@@ -0,0 +1,25 @@
+package com.manyangled.gibbous.optim.convex;
+
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.optim.OptimizationData;
+import org.apache.commons.math3.exception.DimensionMismatchException;
+
+public class EqualityConstraint implements OptimizationData {
+    public final RealMatrix A;
+    public final RealVector b;
+
+    EqualityConstraint(final RealMatrix A, final RealVector b) {
+        int k = A.getColumnDimension();
+        if (b.getDimension() != k)
+            throw new DimensionMismatchException(b.getDimension(), k);
+        this.A = A;
+        this.b = b;
+    }
+
+    EqualityConstraint(final double[][] A, final double[] b) {
+        this(new Array2DRowRealMatrix(A), new ArrayRealVector(b));
+    }
+}
