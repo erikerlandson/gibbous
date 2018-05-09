0914eb5618f665099d3ee9f28db42471d33217c8
Erik Erlandson <eerlands@redhat.com>
2018-05-09 07:15:54 -0700

    tag with OptimizationData

--- src/main/java/com/manyangled/gibbous/optim/convex/KKTSolver.java
+++ src/main/java/com/manyangled/gibbous/optim/convex/KKTSolver.java
@@ -2,6 +2,7 @@ package com.manyangled.gibbous.optim.convex;
 
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.optim.OptimizationData;
 
 // solve block factored matrix equation:
 // | H AT | | v | = -| g |
@@ -9,7 +10,7 @@ import org.apache.commons.math3.linear.RealVector;
 // where (v, w) are primal/dual delta-x and delta-nu from algorithm 10.2
 // This is an abstract class that can be overridden to take advantage of
 // structure in the hessian matrix H, if desired.
-public abstract class KKTSolver {
+public abstract class KKTSolver implements OptimizationData {
     public abstract KKTSolution solve(
         final RealMatrix H,
         final RealMatrix A, final RealMatrix AT,
