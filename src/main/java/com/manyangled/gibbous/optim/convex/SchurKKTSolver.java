2ed5f84820b4585b6f11916a2afc39eca5b94e50
Erik Erlandson <eerlands@redhat.com>
2018-05-09 07:29:19 -0700

    add degenerate case to KKT solution

--- src/main/java/com/manyangled/gibbous/optim/convex/SchurKKTSolver.java
+++ src/main/java/com/manyangled/gibbous/optim/convex/SchurKKTSolver.java
@@ -2,6 +2,7 @@ package com.manyangled.gibbous.optim.convex;
 
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.CholeskyDecomposition;
 import org.apache.commons.math3.linear.DecompositionSolver;
 
@@ -17,14 +18,22 @@ public class SchurKKTSolver {
         final RealVector g, final RealVector h) {
         CholeskyDecomposition cdH = new CholeskyDecomposition(H);
         DecompositionSolver dsH = cdH.getSolver();
-        RealMatrix m1 = dsH.solve(AT);
-        RealVector v1 = dsH.solve(g);
-        RealMatrix S = A.multiply(m1); // -S relative to 10.3
-        CholeskyDecomposition cdS = new CholeskyDecomposition(S);
-        DecompositionSolver dsS = cdS.getSolver();
-        RealVector w = dsS.solve(h.subtract(A.operate(v1))); // both sides negative, so w same
-        RealVector v = dsH.solve(g.add(AT.operate(w))); // this yields -v
-        v.mapMultiplyToSelf(-1.0); // correct -v to +v
-        return new KKTSolution(w, v);
+        if ((A == null) || (A.getRowDimension() < 1)) {
+            // block system is degenerate. A & AT are empty, so
+            // just solve H(v) == -g
+            RealVector v = dsH.solve(g.mapMultiply(-1.0));
+            RealVector w = new ArrayRealVector();
+            return new KKTSolution(v, w);
+        } else {
+            RealMatrix m1 = dsH.solve(AT);
+            RealVector v1 = dsH.solve(g);
+            RealMatrix S = A.multiply(m1); // -S relative to 10.3
+            CholeskyDecomposition cdS = new CholeskyDecomposition(S);
+            DecompositionSolver dsS = cdS.getSolver();
+            RealVector w = dsS.solve(h.subtract(A.operate(v1))); // both sides neg, so w same
+            RealVector v = dsH.solve(g.add(AT.operate(w))); // this yields -v
+            v.mapMultiplyToSelf(-1.0); // correct -v to +v
+            return new KKTSolution(v, w);
+        }
     }
 }
