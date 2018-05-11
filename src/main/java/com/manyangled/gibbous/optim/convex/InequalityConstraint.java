2367f102783bc3ce754cc12383e5355fdbebec4b
Erik Erlandson <eerlands@redhat.com>
2018-05-10 18:16:35 -0700

    InequalityConstraint

---
+++ src/main/java/com/manyangled/gibbous/optim/convex/InequalityConstraint.java
@@ -0,0 +1,38 @@
+/*
+Copyright 2018 Erik Erlandson
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.manyangled.gibbous.optim.convex;
+
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.optim.OptimizationData;
+import org.apache.commons.math3.exception.DimensionMismatchException;
+
+public class InequalityConstraint implements OptimizationData {
+    public final RealMatrix A;
+    public final RealVector b;
+
+    public InequalityConstraint(final RealMatrix A, final RealVector b) {
+        int k = A.getRowDimension();
+        if (b.getDimension() != k)
+            throw new DimensionMismatchException(b.getDimension(), k);
+        this.A = A;
+        this.b = b;
+    }
+
+    public InequalityConstraint(final double[][] A, final double[] b) {
+        this(new Array2DRowRealMatrix(A), new ArrayRealVector(b));
+    }
+}
