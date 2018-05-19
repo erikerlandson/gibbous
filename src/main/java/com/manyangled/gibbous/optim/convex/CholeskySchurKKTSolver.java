4d6f84baeb45ae799ad1d91eb619963a92694c1e
Erik Erlandson <eerlands@redhat.com>
2018-05-19 14:24:41 -0700

    rename SchurKKTSolver to CholeskySchurKKTSolver

--- src/main/java/com/manyangled/gibbous/optim/convex/CholeskySchurKKTSolver.java
+++ src/main/java/com/manyangled/gibbous/optim/convex/CholeskySchurKKTSolver.java
@@ -22,8 +22,7 @@ import org.apache.commons.math3.linear.DecompositionSolver;
 public class SchurKKTSolver extends KKTSolver {
     // step 1 of algorithm 9.5
     public KKTSolution solve(final RealMatrix H, final RealVector g) {
-        CholeskyDecomposition cdH = new CholeskyDecomposition(H);
-        DecompositionSolver dsH = cdH.getSolver();
+        DecompositionSolver dsH = (new CholeskyDecomposition(H)).getSolver();
         RealVector v = dsH.solve(g);
         double lsq = g.dotProduct(v);
         v.mapMultiplyToSelf(-1.0);
@@ -35,13 +34,11 @@ public class SchurKKTSolver extends KKTSolver {
         final RealMatrix H,
         final RealMatrix A, final RealMatrix AT,
         final RealVector g, final RealVector h) {
-        CholeskyDecomposition cdH = new CholeskyDecomposition(H);
-        DecompositionSolver dsH = cdH.getSolver();
+        DecompositionSolver dsH = (new CholeskyDecomposition(H)).getSolver();
         RealMatrix m1 = dsH.solve(AT);
         RealVector v1 = dsH.solve(g);
         RealMatrix S = A.multiply(m1); // -S relative to 10.3
-        CholeskyDecomposition cdS = new CholeskyDecomposition(S);
-        DecompositionSolver dsS = cdS.getSolver();
+        DecompositionSolver dsS = (new CholeskyDecomposition(S)).getSolver();
         RealVector w = dsS.solve(h.subtract(A.operate(v1))); // both sides neg, so w same
         RealVector v = dsH.solve(g.add(AT.operate(w))); // this yields -v
         v.mapMultiplyToSelf(-1.0); // correct -v to +v
